e8ade9d59d25f0d946f28470092f2bef13468371
= <=>
2024-05-10 16:00:56 +0530

    Gopay SDK

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'gopay-sdk/gopay-sdk',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '736da4564fc2a2f2fa082bbd9f864743d2bdfbf1',
+        'reference' => '2a95068182ae0a0b49193e168d41b0da2b751435',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'gopay-sdk/gopay-sdk' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '736da4564fc2a2f2fa082bbd9f864743d2bdfbf1',
+            'reference' => '2a95068182ae0a0b49193e168d41b0da2b751435',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
